349591af7c84dc8dfe4d6866371e43d3a287e41e
kolibri52 <121813005+kolibri52@users.noreply.github.com>
2023-09-18 23:43:30 +0200

    Add files via upload
    Upload of the initial version. Further descriptions will follow.

---
+++ GS1Parser_dotnet/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("GS1Parser")]
+[assembly: AssemblyDescription("GS1 Parser Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jonathan Keller, Github-Repo: kolibri52/GS1Parser")]
+[assembly: AssemblyProduct("GS1Parser_dotnet")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("92b2a73a-e914-4aa8-8d2b-8eb829609f35")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// indem Sie "*" wie unten gezeigt eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
---
+++ TF_ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GS1Parser_dotnet;
+
+namespace TF_ConsoleApp
+{
+    class Program
+    {
+        // Console Test App, just for quick testing purposes
+        static void Main(string[] args)
+        {            
+            string description = "";
+            var parser = new GS1Parser();
+
+            // set a test content
+            string code = "0101234567890128112309152109876543" + (char)29 + "80121.18.3" + (char)29;
+            code = code.Replace('@', (char)29); //to get fnc1
+
+            // parse the test content
+            var result = parser.Parse(code);
+
+            // return each element of the GS1 Code to console
+            foreach (KeyValuePair<string, string> item in result)
+            {
+                Console.WriteLine("AI: {0} ({1}), Value: {2}", item.Key, parser.GetGS1Dict()[item.Key].Description, item.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
